ead8860e0f4dc9ce8d2229f7cf387eedf30b66c2
joaopedromsantos <joaopedromartinscrf2019@gmail.com>
2026-03-25 23:21:14 -0300

    init: initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const address = '0.0.0.0';
+  await app.listen(process.env.PORT ?? 3000, address);
+}
+void bootstrap();
